70ce181bf0347b0e3aa91cc56052dc4adca546b3
mrk5928 <mrk5928@psu.edu>
2023-07-05 20:38:54 -0400

    Mahir: Worked on Python Brute Force Caesar Cipher

--- LM1/Python_Brute Force Ceasar Cipher.py
+++ LM1/Python_Brute Force Ceasar Cipher.py
@@ -1,1 +1,30 @@
-import hashlib
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def decrypt(n, ciphertext):
+    """Decrypt the ciphertext and return the plaintext"""
+    result = ''
+    for l in ciphertext:
+        try:
+            if l.isupper():
+                index = ALPHABET.index(l)
+                i = (index - n) % 26
+                result += ALPHABET[i]
+            else:
+                index = alphabet.index(l)
+                i = (index - n) % 26
+                result += alphabet[i]
+        except ValueError:
+            result += l
+    return result
+
+
+def brute_force(ciphertext):
+    """Perform a brute force attack to find the key and plaintext message"""
+    for key in range(1, 26):
+        plaintext = decrypt(key, ciphertext)
+        print(f"Key: {key}\tPlaintext: {plaintext}")
+
+
+ciphertext = 'Ugew gnwj zwjw Oslkgf'
+brute_force(ciphertext)
