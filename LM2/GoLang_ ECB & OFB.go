71899154b072677d62097e3f68c1793aa9ef3e26
Brenden Anapolsky <bfa5269@psu.edu>
2023-07-12 20:02:56 -0400

    Brenden created/added ECB and OFB

--- LM2/GoLang_ ECB & OFB.go
+++ LM2/GoLang_ ECB & OFB.go
@@ -1,5 +1,90 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strconv"
+)
 
+var codebook = [4][2]int{{0b00, 0b01}, {0b01, 0b10}, {0b10, 0b11}, {0b11, 0b00}}
+
+var iv int = 0b10
+
+func codebookLookup(xor int) int {
+	for i := 0; i < 4; i++ {
+		if codebook[i][0] == xor {
+			return codebook[i][1]
+		}
+	}
+	return 0
+}
+
+func codebookReverseLookup(ciphertext int) int {
+	for i := 0; i < 4; i++ {
+		if codebook[i][1] == ciphertext {
+			return codebook[i][0]
+		}
+	}
+	return 0
+}
+
+func main() {
+	var plaintextStr string
+	fmt.Print("Enter the plaintext (a sequence of 2-bit values): ")
+	fmt.Scan(&plaintextStr)
+
+	plaintextLen := len(plaintextStr)
+	message := make([]int, plaintextLen)
+
+	for i, char := range plaintextStr {
+		bit, err := strconv.Atoi(string(char))
+		if err != nil || (bit != 0 && bit != 1) {
+			fmt.Println("Invalid input. The plaintext should only consist of 0s and 1s.")
+			return
+		}
+		message[i] = bit
+	}
+
+	fmt.Println("ECB encryption details:")
+	fmt.Printf("Plaintext: %b\n", message)
+	ciphertext := make([]int, plaintextLen)
+
+	for i := 0; i < plaintextLen; i++ {
+		ciphertext[i] = codebookLookup(message[i])
+		fmt.Printf("The ciphered value of %b is %b\n", message[i], ciphertext[i])
+	}
+
+	fmt.Println("\nECB decryption details:")
+	decryptedPlaintext := make([]int, plaintextLen)
+
+	for i := 0; i < plaintextLen; i++ {
+		decryptedPlaintext[i] = codebookReverseLookup(ciphertext[i])
+		fmt.Printf("The deciphered value of %b is %b\n", ciphertext[i], decryptedPlaintext[i])
+	}
+
+	fmt.Printf("\nDecrypted ECB message: %b\n", decryptedPlaintext)
+
+	fmt.Println("\nOFB encryption details:")
+	fmt.Printf("Plaintext: %b\n", message)
+	stream := iv
+	ciphertext = make([]int, plaintextLen)
+
+	for i := 0; i < plaintextLen; i++ {
+		xor := message[i] ^ stream
+		ciphertext[i] = codebookLookup(xor)
+		stream = codebookLookup(stream) // Update the stream value using codebook lookup
+		fmt.Printf("The ciphered value of %b is %b\n", message[i], ciphertext[i])
+	}
+
+	fmt.Println("\nOFB decryption details:")
+	stream = iv
+	decryptedPlaintext = make([]int, plaintextLen)
+
+	for i := 0; i < plaintextLen; i++ {
+		xor := ciphertext[i] ^ stream
+		decryptedPlaintext[i] = codebookReverseLookup(xor)
+		stream = codebookLookup(ciphertext[i]) // Update the stream value using codebook lookup
+		fmt.Printf("The deciphered value of %b is %b\n", ciphertext[i], decryptedPlaintext[i])
+	}
+
+	fmt.Printf("\nDecrypted OFB message: %b\n", decryptedPlaintext)
 }
