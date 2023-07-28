67f20c11acf18077231de7824f521a3262fc8f2c
Yuyao Li <yjl5507@psu.edu>
2023-07-27 21:33:00 -0400

    LM4

--- LM4/ChaCha20.go
+++ LM4/ChaCha20.go
@@ -1,5 +1,22 @@
+// LM4: ChaCha20 Programming
 package main
 
+import (
+	//"bufio"
+	"crypto/rand"
+	//"encoding/hex"
+	"fmt"
+	"io"
+	//"os"
+	//"golang.org/x/crypto/chacha20"
+)
+
 func main() {
+	// Generate a random 256-bit key
+	key := make([]byte, 32)
+	if _, err := io.ReadFull(rand.Reader, key); err != nil {
+		fmt.Println("Failed to generate random key:", err)
+		return
+	}
 
 }
