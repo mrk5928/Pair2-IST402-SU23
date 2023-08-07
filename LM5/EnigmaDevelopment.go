747bc7d06f2ee3048910a9e8b76d01e2c65f005e
Mahir Khan <mrk5928@psu.edu>
2023-08-06 21:28:42 -0400

    Mahir - Enigma Development code based on Joe's Example

--- LM5/EnigmaDevelopment.go
+++ LM5/EnigmaDevelopment.go
@@ -1,5 +1,49 @@
+/*
+---------------------------------
+Project: Enigma Development (LM5)
+Name: Mahir Khan
+Class: IST 402
+---------------------------------
+*/
 package main
 
-func main() {
+// Rotor represents a single rotor in the Enigma machine
+type Rotor struct {
+	name            string // Rotor name (e.g., "I", "II", "III", etc.)
+	wiring          string // Rotor wiring configuration
+	turnover        byte   // Turnover position where the next rotor will move
+	currentPosition byte   // Current position of the rotor
+}
 
+// RotorSet represents a set of interconnected rotors in the Enigma machine
+type RotorSet struct {
+	rotors []*Rotor // List of interconnected rotors
+}
+
+// Plugboard represents the plugboard in the Enigma machine
+type Plugboard struct {
+	pairs map[byte]byte // Letter swaps (e.g., 'A' -> 'Z', 'B' -> 'R', etc.)
+}
+
+// Reflector represents the fixed reflector in the Enigma machine
+type Reflector struct {
+	wiring [26]int // Reflector wiring configuration
+}
+
+// InputRotor represents the input rotor in the Enigma machine
+type InputRotor struct {
+	wiring string // InputRotor wiring configuration
+}
+
+// EnigmaMachine represents the complete Enigma machine
+type EnigmaMachine struct {
+	plugboard       Plugboard
+	inputRotor      InputRotor
+	rotorSet        RotorSet
+	reflector       Reflector
+	currentPosition byte // Current position of the rotor set (enables manual setting of the initial position)
+}
+
+func main() {
+	// Code for main function goes here
 }
